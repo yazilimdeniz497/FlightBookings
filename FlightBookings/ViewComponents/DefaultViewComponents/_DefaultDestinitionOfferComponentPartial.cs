5b7ac0e5d6b8647aac7ddac5fbcda89ffbfb6ad4
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-06-10 15:53:39 +0300

    Destinition alanıe eklendi

---
+++ FlightBookings/ViewComponents/DefaultViewComponents/_DefaultDestinitionOfferComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBookings.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultDestinitionOfferComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
