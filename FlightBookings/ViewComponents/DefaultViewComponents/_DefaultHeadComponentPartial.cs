5a7b52d69a797d42a20d2c06a13ed970f039ee1b
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-06-10 11:13:00 +0300

    css yolları ve headcomponent eklendi

---
+++ FlightBookings/ViewComponents/DefaultViewComponents/_DefaultHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBookings.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultHeadComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
