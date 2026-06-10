02d1c2f97d8a9c662e028b5927520ee5e3980dc4
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-06-10 11:58:07 +0300

    booking tags eklendi

---
+++ FlightBookings/ViewComponents/BookingViewComponents/_BookingTagPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBookings.ViewComponents.BookingViewComponents
+{
+    public class _BookingTagPartialComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
