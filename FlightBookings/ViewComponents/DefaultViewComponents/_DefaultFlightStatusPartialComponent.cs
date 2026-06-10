e973d3e7f383cb36e15d16f3c1a62683c4907291
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-06-10 16:47:14 +0300

    flightstatus eklendi

---
+++ FlightBookings/ViewComponents/DefaultViewComponents/_DefaultFlightStatusPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBookings.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultFlightStatusPartialComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
