fb948acd9a9e5a0c9f9cb19b582fb7a7af1ff9c9
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-06-10 15:32:08 +0300

    feature alanı eklendi

---
+++ FlightBookings/ViewComponents/_DefaultFeatureComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBookings.ViewComponents
+{
+    public class _DefaultFeatureComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
