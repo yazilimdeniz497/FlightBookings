c68193fbcf997b4fe4c3ab4af8f2c674a300f347
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-06-10 16:06:27 +0300

    flinvg neext eklendi

---
+++ FlightBookings/ViewComponents/DefaultViewComponents/_DefaultPopulerDestinationPartialComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBookings.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultPopulerDestinationPartialComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
