755d22e972784775fa0e4b67477747cc9bd7a107
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-06-10 15:08:09 +0300

    dosyalar düzenlendi

---
+++ FlightBookings/ViewComponents/BookingViewComponents/_BookingAreaPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBookings.ViewComponents.BookingViewComponents
+{
+    public class _BookingAreaPartialComponent:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
