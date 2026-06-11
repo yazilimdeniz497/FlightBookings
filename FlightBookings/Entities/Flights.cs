b7fd1d08a87285f36a659c285fbafe09a3e25866
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-06-11 11:07:48 +0300

    flights modeli eklendi

---
+++ FlightBookings/Entities/Flights.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightBookings.Entities
+{
+    public class Flights
+    {
+        [Key]
+        public int FlightId { get; set; }
+        public string FlightNumber { get; set; }
+        public string AirlineNumber { get; set; }
+    }
+}
