e1cb1d613c8a6d150dd39190f8a3b1b578eb8e75
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-06-11 08:22:28 +0300

    sıkça sorulan sorular ve scriptler eklendi

---
+++ FlightBookings/ViewComponents/DefaultViewComponents/_DefaultScriptComponentPartial.cs
@@ -0,0 +1,6 @@
+﻿namespace FlightBookings.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultScriptComponentPartial
+    {
+    }
+}
---
+++ FlightBookings/ViewComponents/DefaultViewComponents/_DefaultServiceComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBookings.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultServiceComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
