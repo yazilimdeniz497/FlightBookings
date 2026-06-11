e53e99381067eebfae88631619e1dfb3e86cb498
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-06-11 11:23:49 +0300

    flight model eklendi

--- FlightBookings/Entities/Flights.cs
+++ FlightBookings/Entities/Flights.cs
@@ -8,5 +8,15 @@ namespace FlightBookings.Entities
         public int FlightId { get; set; }
         public string FlightNumber { get; set; }
         public string AirlineNumber { get; set; }
+        public string DepartureAirportCode { get; set; }
+       public string DepartureAirportName { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public int DurationMinutes { get; set; }
+        public int TotalSeats { get; set; }
+        public int CapacitySeats { get; set; }
+        public decimal BasePrice { get; set; }
+        public string Currency { get; set; }
+        public string Status{ get; set; }
     }
 }
