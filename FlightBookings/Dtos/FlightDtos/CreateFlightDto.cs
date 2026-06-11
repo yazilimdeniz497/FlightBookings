4f62e7a3ddcbcde82f90ce320dac8938d58de635
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-06-11 11:34:56 +0300

    dto classları eklendi

---
+++ FlightBookings/Dtos/FlightDtos/CreateFlightDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FlightBookings.Dtos.FlightDtos
+{
+    public class CreateFlightDto
+    {
+      
+        public string FlightNumber { get; set; }
+        public string AirlineNumber { get; set; }
+        public string DepartureAirportCode { get; set; }
+        public string DepartureAirportName { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+        public int DurationMinutes { get; set; }
+        public int TotalSeats { get; set; }
+        public int CapacitySeats { get; set; }
+        public decimal BasePrice { get; set; }
+        public string Currency { get; set; }
+        public string Status { get; set; }
+    }
+}
