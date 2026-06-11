d5c5461ad8d0dbc6bcf43b9899df5a58c312c7b8
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-06-11 10:50:01 +0300

    sidebar ve topbar eklendi

--- FlightBookings/Areas/Admin/Controllers/FlightsController.cs
+++ FlightBookings/Areas/Admin/Controllers/FlightsController.cs
@@ -14,11 +14,7 @@ namespace FlightBookings.Areas.Admin.Controllers
         {
             return View();
         }
-        [HttpPost]
-        public IActionResult CreateFlight()
-        {
-            return View();
-        }
+       
 
     }
 }
---
+++ FlightBookings/Areas/Admin/ViewComponents/AdminTopbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBookings.Areas.Admin.ViewComponents
+{
+    public class AdminTopbarComponentPartial: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
